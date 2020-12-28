d55b42bc71ff3c9b821e5dd34bbbfca6eebfe01b
AlexisLoya <alexloy117@gmail.com>
2020-12-28 00:16:24 -0600

    getting cookies for response

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect
 
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    user_ip = request.remote_addr
+    #Redireccionar
+    response = make_response(redirect('/hello'))
+    #agregar cookie al response
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+
+@app.route('/hello')
 def hello():
-    return 'Hello flask'
+    #tomar cookie del response
+    user_ip = request.cookies.get('user_ip')
+    return f'Hello flask tu ip es  {user_ip}'
