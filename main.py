d7a5bb019e6ba059ffe398aee875239db1869e30
AlexisLoya <alexloy117@gmail.com>
2020-12-29 13:35:29 -0600

    manejo de errores

--- main.py
+++ main.py
@@ -5,8 +5,19 @@ app = Flask(__name__)
 
 todos = ['Comprar café', 'Enviar solicitud de compra', 'Entregar video a producción']
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html', error=error)
+
+
 @app.route('/')
 def index():
+    #raise(Exception('500 error')) # provocar un error 500
     user_ip = request.remote_addr
     #Redireccionar
     response = make_response(redirect('/hello'))
