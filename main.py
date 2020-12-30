9cc1e2defff218e75bc28dcbe7b12c64106e4b0a
AlexisLoya <alexloy117@gmail.com>
2020-12-30 11:27:20 -0600

    segmentando el proyecto y agregando blueprints

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
-from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from flask import request, make_response, redirect, render_template, session, url_for, flash
 
-app = Flask(__name__)
-#Inicializar bootstrap
-bootstrap = Bootstrap(app)
-
-#key session
-app.config['SECRET_KEY'] = 'SUPER SECRETO'
-
-#Clase formulario
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de usuario', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
+import unittest
+from app import create_app
+from app.forms import LoginForm
+app = create_app()
 
 todos = ['Comprar café', 'Enviar solicitud de compra', 'Entregar video a producción']
+
+
+
+@app.cli.command()
+def test():
+    test = unittest.TestLoader().discover('test')
+    unittest.TextTestRunner().run(test)
+
 
 @app.errorhandler(404)
 def not_found(error):
